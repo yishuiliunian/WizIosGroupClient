921063f97574b622afc9e51cc36df6180404a753
wzz <wzz@wiz.cn>
2012-11-13 15:37:14 +0800

    Feedback-interface1

---
+++ FeedBackViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedBackViewController.h
+//  WizGroup
+//
+//  Created by wiz on 12-11-13.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedBackViewController : UINavigationController
+
+@end
---
+++ WGFeedBackViewCell.m
@@ -0,0 +1,13 @@
+//
+//  WGFeedBackViewCell.m
+//  WizGroup
+//
+//  Created by wiz on 12-11-13.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import "WGFeedBackViewCell.h"
+
+@implementation WGFeedBackViewCell
+
+@end
