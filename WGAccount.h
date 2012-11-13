1b7d25ec53c742d9e8a935a729e77ef2f7bb5824
wzz <wzz@wiz.cn>
2012-11-13 18:06:12 +0800

    Feedback-jsonwrite

---
+++ WGAccount.h
@@ -0,0 +1,13 @@
+//
+//  WGAccount.h
+//  WizGroup
+//
+//  Created by wiz on 12-11-13.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WGAccount : NSObject
+
+@end
---
+++ WGAccount.m
@@ -0,0 +1,13 @@
+//
+//  WGAccount.m
+//  WizGroup
+//
+//  Created by wiz on 12-11-13.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import "WGAccount.h"
+
+@implementation WGAccount
+
+@end
