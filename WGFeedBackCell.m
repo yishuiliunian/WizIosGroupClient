00c28f75720734202a13278a06e45bac85202032
wzz <wzz@wiz.cn>
2012-11-14 15:47:42 +0800

    Feedback-local

---
+++ WGFeedBackCell.m
@@ -0,0 +1,29 @@
+//
+//  WGFeedBackCell.m
+//  WizGroup
+//
+//  Created by wiz on 12-11-14.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import "WGFeedBackCell.h"
+
+@implementation WGFeedBackCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
